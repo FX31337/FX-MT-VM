58335dc8c203ceef7dd29288153287894f1eda9d
kenorb <kenorb@gmail.com>
2015-10-03 01:33:00 +0100

    Initial Vagrantfile and scripts/provision.sh files.

---
+++ Vagrantfile
@@ -0,0 +1,56 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+require 'getoptlong'
+
+=begin
+
+# @todo: http://stackoverflow.com/q/14124234/55075
+# Parse optional arguments.
+opts = GetoptLong.new(
+  [ '--file-ea',  GetoptLong::OPTIONAL_ARGUMENT ], # EA file.
+  [ '--dir-bt',   GetoptLong::OPTIONAL_ARGUMENT ], # Dir with backtest files.
+  [ '--dir-sets', GetoptLong::OPTIONAL_ARGUMENT ]  # Dir with set files.
+)
+
+opts.each do |opt, arg|
+  case opt
+    when '--file-ea'
+      file_ea==arg
+    when '--dir-bt'
+      dir_bt=arg
+    when '--dir-sets'
+      dir_sets=arg
+  end
+end
+
+# @todo: When implementing above, please make sure that running of: 'vagrant -f destroy' would be supported (no invalid option error is shown).
+
+=end
+
+# Vagrantfile API/syntax version.
+API_VERSION = "2"
+
+Vagrant.configure(API_VERSION) do |config|
+
+  config.vm.box = "ubuntu/vivid64"
+  config.vm.box_version = "20150722.0.0"
+  config.vm.network "private_network", ip: "192.168.22.22"
+  config.vm.hostname = "vagrant"
+  config.vm.provision "shell",
+    path: "scripts/provision.sh"
+    # :args => '--file-ea' + opt['--file-ea'].to_s + ' --dir-bt' + opt['--dir-bt'].to_s + ' --dir-sets' + opt['--dir-sets'].to_s # @todo
+ #config.ssh.pty = true # Use pty for provisioning. Could hang the script.
+  config.ssh.forward_agent = true # Enables agent forwarding over SSH connections.
+  config.ssh.forward_x11 = true # Enables X11 forwarding over SSH connections.
+
+  config.vm.synced_folder ".", "/vagrant", id: "core",
+    nfs: true
+
+  config.vm.provider "virtualbox" do |v|
+    v.customize ['modifyvm', :id, '--natdnshostresolver1', 'on']
+    v.memory = 2048
+    v.cpus = 2
+  end
+
+end
---
+++ scripts/provision.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+#
+# Provisioning script
+#
+
+# Initialize script.
+set -xe
+if [ ! -d "/vagrant/scripts" ]; then
+  echo "This script needs to be run within vagrant VM."
+  exit 1
+fi
+
+whoami && pwd
+shopt -s globstar # Enable globbing.
+
+# Perform an unattended installation of a Debian packages.
+ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
+dpkg-reconfigure debconf -f noninteractive -p critical
+
+# Install the locale packate to prevent an invalid locale.
+apt-get install -y language-pack-en
+
+# Install basic utils.
+apt-get install -y links html2text tree
+
+# Install and run X virtual framebuffer.
+apt-get install -y Xvfb xdotool
+
+# Install wine
+dpkg --add-architecture i386
+add-apt-repository -y ppa:ubuntu-wine
+apt-get update
+apt-get install -y wine # wine-gecko2.36\* wine-mono4.5.6\* winbind
+
+# Upgrade manually some packages from the source.
+apt-get install -y libx11-dev libxtst-dev libxinerama-dev libxkbcommon-dev
+
+# Install composer (https://getcomposer.org/) via PHP.
+#apt-get install php5-cli
+#curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install xdotool.
+#git clone https://github.com/jordansissel/xdotool && make -C xdotool
+
+# Run X virtual framebuffer on screen 0.
+Xvfb :0 -screen 0 1024x768x16 & # Run X virtual framebuffer on screen 0.
+
+# Set-up git.
+git config --system user.name "Vagrant"
+git config --system user.email "vagrant@localhost"
+git config --system core.sharedRepository group
+
+# Add version control for /opt.
+git init /opt
+
+# Give vagrant write permission for /opt.
+chown -R vagrant:vagrant /opt
+
+# Install VM specific binaries.
+#install -v /vagrant/scripts/run_backtest.sh /usr/local/bin/run_backtest
+#install -v /vagrant/scripts/run_optimizer.sh /usr/local/bin/run_optimizer
+
+# Append extra settings into bashrc file.
+#ex +':$s@$@\ralias run_backtest=/vagrant/scripts/run_backtest.sh@' -cwq /etc/bash.bashrc
+
+echo "$0 done."
